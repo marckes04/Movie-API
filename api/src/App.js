113b846b195d1cba6e9f4c5bdf9a8f381b948b7d
marckes04 <maresp95@gmail.com>
2022-09-12 23:07:08 -0500

    first commit

---
+++ api/src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom" 
+import Header from './components/header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header/>
+        <Routes>
+          <Route index element = {<h1>hello world 2</h1>}> </Route>
+          <Route path="movie/:id" element={<h1>Movie detail Page</h1>}></Route>
+          <Route path="movies/:type" element={<h1>Movie list Page</h1>}></Route>
+          <Route path="/*" element= {<h1>Error Page</h1>}></Route>
+        
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
